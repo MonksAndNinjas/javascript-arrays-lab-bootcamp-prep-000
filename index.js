e7166a917bc049dacd21377b050703a26b720ff2
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-03-23 06:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,3 +46,9 @@ function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1)
   kittens === ["Milo", "Otis", "Garfield"]
 }
+
+kittens = ["Milo", "Otis", "Garfield"]
+
+function removeFirstKitten() {
+  return kittens.slice()
+}
